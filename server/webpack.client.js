6328aee0bec68a3888efa80267348399ac955fd2
Antoine <solnichkin.antoine@gmail.com>
2018-11-17 12:43:05 +0100

    added google analytics plus various webpacks optimizations

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,7 +1,8 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base.js");
-
+const CompressionPlugin = require("compression-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const config = {
   // Tell webpack the root file of our client app
   entry: "./src/client/client.js",
@@ -10,6 +11,16 @@ const config = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
   },
+  plugins: [new CompressionPlugin({ cache: true })],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        test: /\.js$|\.css$|\.html$/,
+      })
+    ]
+  }
 };
 
 module.exports = merge(baseConfig, config);
