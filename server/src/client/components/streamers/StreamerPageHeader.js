da33d0bc9142446d745a90c8ce3e9a5f80dfa02a
Antoine <solnichkin.antoine@gmail.com>
2018-11-17 10:06:14 +0100

    Styling on the body and streamer page header

--- server/src/client/components/streamers/StreamerPageHeader.js
+++ server/src/client/components/streamers/StreamerPageHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Segment, Item, Label, Icon } from "semantic-ui-react";
 import { connect } from "react-redux";
-import StreamerDetailPageHelmet from '../seo/StreamerDetailPageHelmet'
+import StreamerDetailPageHelmet from "../seo/StreamerDetailPageHelmet";
 
 const mapStateToProps = state => ({
   streamer: state.ui.selectedStreamer
@@ -10,55 +10,55 @@ const mapStateToProps = state => ({
 const StreamerPageHeader = ({ streamer, numberOfClips }) => {
   return (
     <div>
-    {streamer && <StreamerDetailPageHelmet streamer={streamer.displayName} />}
-    <Segment className="bordered-segment">
-      {streamer && (
-        <Item.Group>
-          <Item>
-            <Item.Image size="tiny" src={streamer.profileImageURL} />
-            <Item.Content>
-              <Item.Header>{streamer.displayName}</Item.Header>
-              <Item.Meta>
-                {streamer.description
-                  ? streamer.description
-                  : "Apparently, this streamer prefers to keep an air of mystery about them"}
-              </Item.Meta>
-              <Item.Meta>
-                {streamer.monitored ? (
+      {streamer && <StreamerDetailPageHelmet streamer={streamer.displayName} />}
+      <Segment className="bordered-segment">
+        {streamer && (
+          <Item.Group>
+            <Item>
+              <Item.Image size="tiny" src={streamer.profileImageURL} />
+              <Item.Content>
+                <Item.Header>{streamer.displayName}</Item.Header>
+                <Item.Meta>
+                  {streamer.description
+                    ? streamer.description
+                    : "Apparently, this streamer prefers to keep an air of mystery about them"}
+                </Item.Meta>
+                <Item.Meta>
+                  {streamer.monitored ? (
+                    <Label>
+                      <Icon
+                        name="circle thin"
+                        color="red"
+                        style={{ marginRight: "10px" }}
+                      />
+                      Live
+                    </Label>
+                  ) : (
+                    <Label>
+                      <Icon
+                        name="circle thin"
+                        color="grey"
+                        style={{ marginRight: "10px" }}
+                      />
+                      Disconnected
+                    </Label>
+                  )}
+                </Item.Meta>
+                <Item.Meta>
                   <Label>
                     <Icon
-                      name="circle thin"
-                      color="red"
+                      name="twitch"
+                      color="violet"
                       style={{ marginRight: "10px" }}
                     />
-                    Live
+                    {streamer.broadcasterType}
                   </Label>
-                ) : (
-                  <Label>
-                    <Icon
-                      name="circle thin"
-                      color="grey"
-                      style={{ marginRight: "10px" }}
-                    />
-                    Disconnected
-                  </Label>
-                )}
-              </Item.Meta>
-              <Item.Meta>
-                <Label>
-                  <Icon
-                    name="twitch"
-                    color="violet"
-                    style={{ marginRight: "10px" }}
-                  />
-                  {streamer.broadcasterType}
-                </Label>
-              </Item.Meta>
-            </Item.Content>
-          </Item>
-        </Item.Group>
-      )}
-    </Segment>
+                </Item.Meta>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        )}
+      </Segment>
     </div>
   );
 };
